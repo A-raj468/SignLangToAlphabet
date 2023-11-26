ab3ec56a3c28b59eb0f8807b235e1d9b65ea8551
Aditya Raj <adityaraj6508@gmail.com>
2023-11-26 20:18:42 +0530

    Added on screen prompts and cleaned the code

--- inference_classifier.py
+++ inference_classifier.py
@@ -22,16 +22,41 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-# labels_dict = {0: 'B', 1: 'A', 2: 'L'}
-# labels_dict = {0:"a",1:"b",2:"c",3:"d",4:"e",5:"f",6:"g",7:"h",8:"i",9:"j",10:"k",11:"l",12:"m",13:"n",14:"o",15:"p",16:"q",17:"r",18:"s",19:"t",20:"u",21:"unkowen",22:"v",23:"w",24:"x",25:"y",26:"z"}
 labels_dict = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i", 9: "j", 10: "k", 11: "l", 12: "m", 13: "n",
-               14: "o", 15: "p", 16: "q", 17: "r", 18: "s", 19: "t", 20: "u", 21: "unkowen", 22: "v", 23: "w", 24: "x", 25: "y", 26: "z"}
+               14: "o", 15: "p", 16: "q", 17: "r", 18: "s", 19: "t", 20: "u", 21: "v", 22: "w", 23: "x", 24: "y", 25: "z"}
 
-
-# while True:
 i = 0
 start_time = time.time()
+prompt_lines = ['Press "C" to capture!', 'Press "Q" to quit!']
+
+# Font settings
+font = cv2.FONT_HERSHEY_COMPLEX
+font_scale = 0.75
+font_thickness = 3
+font_color = (128, 0, 0)  # Green in BGR format
+line_height = 30  # Adjust this value to set the vertical spacing between lines
+
 while cap.isOpened():
+    tobrk = False
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+
+        # Initial coordinates
+        x, y = 50, 50
+        for prompt in prompt_lines:
+            cv2.putText(frame, prompt, (x, y), font, font_scale,
+                        font_color, font_thickness, cv2.LINE_AA)
+            y += line_height
+        cv2.imshow('frame', frame)
+        key = cv2.waitKey(25)
+        if key == ord('c'):
+            break
+        elif key == ord('q'):
+            tobrk = True
+            break
+    if tobrk:
+        break
     elapsed_time = time.time() - start_time
     data_aux = []
     x_ = []
@@ -47,33 +72,6 @@ while cap.isOpened():
 
     results = hands.process(frame_rgb)
     if results.multi_hand_landmarks:
-        # for hand_landmarks in results.multi_hand_landmarks:
-        #     mp_drawing.draw_landmarks(
-        #         frame,  # image to draw
-        #         hand_landmarks,  # model output
-        #         mp_hands.HAND_CONNECTIONS,  # hand connections
-        #         mp_drawing_styles.get_default_hand_landmarks_style(),
-        #         mp_drawing_styles.get_default_hand_connections_style())
-
-        # for hand_landmarks in results.multi_hand_landmarks:
-        #     for i in range(len(hand_landmarks.landmark)):
-        #         x = hand_landmarks.landmark[i].x
-        #         y = hand_landmarks.landmark[i].y
-
-        #         x_.append(x)
-        #         y_.append(y)
-
-        #     for i in range(len(hand_landmarks.landmark)):
-        #         x = hand_landmarks.landmark[i].x
-        #         y = hand_landmarks.landmark[i].y
-        #         data_aux.append(x - min(x_))
-        #         data_aux.append(y - min(y_))
-
-        # x1 = int(min(x_) * W) - 10
-        # y1 = int(min(y_) * H) - 10
-
-        # x2 = int(max(x_) * W) - 10
-        # y2 = int(max(y_) * H) - 10
         for landmarks in results.multi_hand_landmarks:
             # Get bounding box coordinates
             x_min, y_min = int(min(landmark.x for landmark in landmarks.landmark) * frame.shape[1]), \
@@ -87,6 +85,9 @@ while cap.isOpened():
             x_min = int(x_min*0.8)
             x_max = int(x_max*1.2)
             hand_image = frame[y_min:y_max, x_min:x_max]
+
+        elapsed_time = 100
+
         if elapsed_time >= 1 and hand_image.shape[0] > 0 and hand_image.shape[1] > 0:
             start_time = time.time()
             # Display the extracted hand image
@@ -98,33 +99,32 @@ while cap.isOpened():
             hand_image_gray = np.stack([hand_image_gray] * 3)
             hand_image_gray = hand_image_gray[np.newaxis, :, :, :]
             # hand_image_gray = np.array([hand_image_gray] * 3)
-            # hand_image_gray = np.transpose(hand_image_gray, (1, 2, 0))
-            # print(type(hand_image_gray))
+            # hand_image_gray = np.transpose(hand_image_gray, (0, 3, 1, 2))
             # print(hand_image_gray.shape)
+            # print(type(hand_image_gray))
             # use model and predict
 
             outputs = model(torch.tensor(hand_image_gray))
-            _, predicted = torch.max(outputs, 1)
+            probs = torch.nn.functional.softmax(outputs, dim=1)
+            # print(probs)
+            _, predicted = torch.max(probs, 1)
 
-            print(f"{i}: {labels_dict[predicted.item()]}")
+            confidence = probs[0, predicted].item() * 100
+            alphabet = labels_dict[predicted.item()]
+            print(f"{i}: {alphabet}")
             i += 1
 
-            # print(type(hand_image_gray))
-            cv2.putText(hand_image_gray, "S", (5, 45),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
-            # cv2.imshow('Hand Image', hand_image_gray)
-
-            # outputs = model(torch.tensor(data_aux))labels_dict[predicted.item()]
-
-            # prediction = torch.argmax(outputs).item()
-
-            # predicted_character = labels_dict[prediction]
+            # if len(prompt_lines) < 2:
+            #     word = alphabet
+            # else:
+            #     word += alphabet
+            prompt_lines = [f'Alphabet: {alphabet}',
+                            f'Confidence: {confidence: .2f}%',
+                            # f'Word till now: {word}',
+                            'Press "C" to capture again!',
+                            'Press "Q" to quit!',]
 
     cv2.imshow('frame', frame)
-    # cv2.waitKey(1)
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
 
 
 cap.release()
